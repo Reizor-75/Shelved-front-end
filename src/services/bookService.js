cb1eaec222829e89edf2d78e189d3167e7874c20
mlinares <m.linares93@gmail.com>
2024-03-25 22:58:17 -0400

    create getAllBooks service function

--- src/services/bookService.js
+++ src/services/bookService.js
@@ -3,6 +3,17 @@ import * as tokenService from './tokenService'
 
 const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/api/books`
 
-export { 
+async function getAllBooks() {
+  try {
+    const res = await fetch(BASE_URL, {
+      headers: { 'Authorization': `Bearer ${tokenService.getToken()}` },
+    })
+    return await res.json()
+  } catch (err) {
+    throw new Error(err)
+  }
+}
 
+export { 
+  getAllBooks,
 }
