60c1ee936197e52b9c6621f4284fd7f902fca83f
mlinares <m.linares93@gmail.com>
2024-03-25 23:26:47 -0400

    setup books data

--- src/pages/Books/Books.jsx
+++ src/pages/Books/Books.jsx
@@ -1,3 +1,6 @@
+// npm modules
+import { useState, useEffect } from 'react'
+
 // services
 import * as bookService from '../../services/bookService'
 
@@ -5,9 +8,27 @@ import * as bookService from '../../services/bookService'
 import styles from './Books.module.css'
 
 const Books = () => {
+  const [books, setBooks] = useState([])
+
+  useEffect(() => {
+    const fetchProfiles = async () => {
+      const booksData = await bookService.getAllBooks()
+      setBooks(booksData)
+    }
+    fetchProfiles()
+  }, [])
+
+  if (!books.length) {
+    return <main className={styles.container}><h1>Loading Books..</h1></main>
+  }
+  
+
   return (  
     <main className={styles.container}>
       <h1>Books</h1>
+      {books.map(book => (
+        <p key={book._id}>{book.title}</p>
+      ))}
     </main>
   );
 }
