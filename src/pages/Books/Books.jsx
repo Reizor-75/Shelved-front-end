cf1fbfddc3a182ee5ca942e8c95eab2d9ffd6bf0
mlinares <m.linares93@gmail.com>
2024-03-25 23:05:11 -0400

    create Books Page Template

---
+++ src/pages/Books/Books.jsx
@@ -0,0 +1,15 @@
+// services
+import * as profileService from '../../services/booksService'
+
+// css
+import styles from './Books.module.css'
+
+const Books = () => {
+  return (  
+    <main className={styles.container}>
+      <h1>Books</h1>
+    </main>
+  );
+}
+
+export default Books;
