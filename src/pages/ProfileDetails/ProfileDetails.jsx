4a2b6542c8b4b853e139a80d11e55ce070bd59b3
mlinares <m.linares93@gmail.com>
2024-03-27 21:20:59 -0400

    setup profileDetails view

---
+++ src/pages/ProfileDetails/ProfileDetails.jsx
@@ -0,0 +1,13 @@
+
+// css
+import styles from './ProfileDetails.module.css'
+
+const ProfileDetails = () => {
+  return (  
+    <main className={styles.container}>
+
+    </main>
+  );
+}
+
+export default ProfileDetails;
