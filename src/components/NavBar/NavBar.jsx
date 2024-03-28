018ab6469266c8050b8c70934a679a1c9c32a8f6
mlinares <m.linares93@gmail.com>
2024-03-27 21:06:04 -0400

    setup navBar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,24 +1,35 @@
 // npm modules
 import { NavLink } from 'react-router-dom'
 
+// css
+import styles from './NavBar.module.css'
+
 const NavBar = ({ user, handleLogout }) => {
   return (
-    <nav>
-      {user ?
-        <ul>
-          <li>Welcome, {user.name}</li>
-          <li><NavLink to="/profiles">Profiles</NavLink></li>
-          <li><NavLink to="" onClick={handleLogout}>LOG OUT</NavLink></li>
-          <li><NavLink to="/auth/change-password">Change Password</NavLink></li>
-          <li><NavLink to="/books">Books</NavLink></li>
-        </ul>
-      :
-        <ul>
-          <li><NavLink to="/auth/login">Log In</NavLink></li>
-          <li><NavLink to="/auth/signup">Sign Up</NavLink></li>
-          <li><NavLink to="/books">Books</NavLink></li>
-        </ul>
-      }
+    <nav className={styles.navContainer}>
+      <div className={styles.logoContainer}>
+        <img src="" alt="Shelved Logo"  className={styles.logoImage}/>
+        <div className={styles.logoTitle}>Shelved</div>
+      </div>
+      <div className={styles.links}>
+        <NavLink to="/books">Books</NavLink>
+        {user ?
+          <ul>
+            <li>Welcome, {user.name}</li>
+            <li><NavLink to="/profiles">Profiles</NavLink></li>
+            <li><NavLink to="" onClick={handleLogout}>LOG OUT</NavLink></li>
+            <li><NavLink to="/auth/change-password">Change Password</NavLink></li>
+          </ul>
+        :
+          <div className={styles.userLinks}>
+            <img src="" alt="UserAvatar" />
+            <div className={styles.userDropDown}>
+              <NavLink to="/auth/login">Log In</NavLink>
+              <NavLink to="/auth/signup">Sign Up</NavLink>
+            </div>
+          </div>
+        }
+      </div>
     </nav>
   )
 }
