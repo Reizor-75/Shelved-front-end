51ae903a73de8d7def3ead5b51928705e871406a
mlinares <m.linares93@gmail.com>
2024-03-28 21:49:24 -0400

    setup search view

---
+++ src/pages/Search/Search.jsx
@@ -0,0 +1,13 @@
+
+// css
+import styles from './ProfileDetails.module.css'
+
+const Search = () => {
+  return (  
+    <main className={styles.container}>
+      <h1>Search</h1>
+    </main>
+  );
+}
+
+export default Search;
