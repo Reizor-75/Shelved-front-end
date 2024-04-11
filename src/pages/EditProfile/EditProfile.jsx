7bb83bd6a6e584ecf47789bf9cef7296fcba33f2
mlinares <m.linares93@gmail.com>
2024-04-10 22:59:37 -0400

    update styling for forms

--- src/pages/EditProfile/EditProfile.jsx
+++ src/pages/EditProfile/EditProfile.jsx
@@ -1,34 +1,106 @@
 // npm modules
-import { useState } from 'react'
-import { useLocation } from "react-router-dom"
+import { useState, useRef } from 'react'
+import { Link, useNavigate, useLocation } from 'react-router-dom'
+
+// services
+import * as authService from '../../services/authService'
 
 // css
 import styles from './EditProfile.module.css'
 
-const EditProfile = () => {
+const EditProfile = ({ handleAuthEvt }) => {
+  const navigate = useNavigate()
+  const imgInputRef = useRef(null)
+
   const { state } = useLocation()
   const [formData, setFormData] = useState(state)
   
-  const handleChange = (evt) => {
-    setFormData({ ...formData, [evt.target.name]: evt.target.value })
-  }    
+  const [message, setMessage] = useState('')
+  const [photoData, setPhotoData] = useState({ photo: null })
+  const [isSubmitted, setIsSubmitted] = useState(false)
 
-  const handleSubmit = (evt) => {
+  const handleChange = evt => {
+    setMessage('')
+    setFormData({ ...formData, [evt.target.name]: evt.target.value })
+  }
+
+  const handleChangePhoto = evt => {
+    const file = evt.target.files[0]
+    let isFileInvalid = false
+    let errMsg = ""
+    const validFormats = ['gif', 'jpeg', 'jpg', 'png', 'svg', 'webp']
+    const photoFormat = file.name.split('.').at(-1)
+
+    // cloudinary supports files up to 10.4MB each as of May 2023
+    if (file.size >= 10485760) {
+      errMsg = "Image must be smaller than 10.4MB"
+      isFileInvalid = true
+    }
+    if (!validFormats.includes(photoFormat)) {
+      errMsg = "Image must be in gif, jpeg/jpg, png, svg, or webp format"
+      isFileInvalid = true
+    }
+    
+    setMessage(errMsg)
+    
+    if (isFileInvalid) {
+      imgInputRef.current.value = null
+      return
+    }
+
+    setPhotoData({ photo: evt.target.files[0] })
+  }
+
+  const handleSubmit = async evt => {
     evt.preventDefault()
+    try {
+      if (!import.meta.env.VITE_BACK_END_SERVER_URL) {
+        throw new Error('No VITE_BACK_END_SERVER_URL in front-end .env')
+      }
+      setIsSubmitted(true)
+      await authService.signup(formData, photoData.photo)
+      handleAuthEvt()
+      navigate('/')
+    } catch (err) {
+      console.log(err)
+      setMessage(err.message)
+      setIsSubmitted(false)
+    }
+  }
+
+  const { name } = formData
+
+  const isFormInvalid = () => {
+    return !(name)
   }
 
   return (  
     <main className={styles.container}>
       <div>Edit {formData.name}'s Profile </div>
-      <form autoComplete="off"  onSubmit={handleSubmit}>
-      <label className={styles.label}>
-          Name
-          <input type="text" value={formData.name} name="name" onChange={handleChange} />
-        </label> 
+      <p className={styles.message}>{message}</p>
+      <form autoComplete="off" onSubmit={handleSubmit} className={styles.form}>
         <label className={styles.label}>
-          Photo
-          <input type="text" value={formData.photo} name="photo" onChange={handleChange} />
-        </label> 
+          Name
+          <input type="text" value={name} name="name" onChange={handleChange} />
+        </label>
+        <label className={styles.label}>
+          Upload Photo
+          <input 
+            type="file" 
+            name="photo" 
+            onChange={handleChangePhoto}
+            ref={imgInputRef}
+          />
+        </label>
+        <div>
+          <Link to="/">Cancel</Link>
+          <button
+            className={styles.button}
+            disabled={ isFormInvalid() || isSubmitted }
+          >
+            {!isSubmitted ? 'Update' : '🚀 Sending...'}
+          </button>
+        </div>
       </form>
     </main>
   );
