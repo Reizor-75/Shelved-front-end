e3ae34fad4517a0ec9b09837b3cae4223fa1001a
mlinares <m.linares93@gmail.com>
2024-04-10 22:38:04 -0400

    update styling of navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -10,60 +10,62 @@ import shelvedLogo from "../../assets/Shelved_Logo.svg"
 const NavBar = ({ user, handleLogout }) => {
 
   return (
-    <nav className={styles.navContainer}>
-      <div className={styles.logoContainer}>
-        <img src={shelvedLogo} alt="Shelved Logo" width='100px' className={styles.logoImage}/>
-        <div className={styles.logoTitle}>Shelved</div>
-      </div>
-      <div className={styles.links}>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/books">Books</NavLink>
-        <NavLink to="/search">Search</NavLink>
-        {user ?
-          <div className={styles.userLinks}>
+    <nav>
+      <div className={styles.navContainer}>
+        <div className={styles.logoContainer}>
+          <img src={shelvedLogo} alt="Shelved Logo" width='100px' className={styles.logoImage}/>
+          <div className={styles.logoTitle}>Shelved</div>
+        </div>
+        <div className={styles.links}>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/books">Books</NavLink>
+          <NavLink to="/search">Search</NavLink>
+          {user ?
+            <div className={styles.userLinks}>
+                <i className="fa-solid fa-circle-user"></i>
+
+              <div className={styles.userDropDown}>
+                <NavLink to={`/profiles/${user.profile}`}>My Profile</NavLink>
+                <NavLink to="/auth/change-password">Change Password</NavLink>
+                <NavLink to="" onClick={handleLogout}>LOG OUT</NavLink>
+              </div>
+            </div>
+          :
+            <div className={styles.userLinks}>
+              {/* <img src="" alt="UserAvatar" /> */}
               <i className="fa-solid fa-circle-user"></i>
+              <div className={styles.userDropDown}>
+                <NavLink to="/auth/login">Log In</NavLink>
+                <NavLink to="/auth/signup">Sign Up</NavLink>
+              </div>
+            </div>
+          }
+        </div>
 
-            <div className={styles.userDropDown}>
-              <NavLink to={`/profiles/${user.profile}`}>My Profile</NavLink>
-              <NavLink to="/auth/change-password">Change Password</NavLink>
-              <NavLink to="" onClick={handleLogout}>LOG OUT</NavLink>
-            </div>
-          </div>
-        :
-          <div className={styles.userLinks}>
-            {/* <img src="" alt="UserAvatar" /> */}
-            <i className="fa-solid fa-circle-user"></i>
-            <div className={styles.userDropDown}>
-              <NavLink to="/auth/login">Log In</NavLink>
-              <NavLink to="/auth/signup">Sign Up</NavLink>
-            </div>
-          </div>
-        }
-      </div>
+        <div className={styles.linksMobile}>
+          {user ?
+            <div className={styles.userLinks}>
+              <i className="fa-solid fa-bars"></i>
 
-      <div className={styles.linksMobile}>
-        {user ?
-          <div className={styles.userLinks}>
-            <i className="fa-solid fa-bars"></i>
-
-            <div className={styles.userDropDown}>
-              <NavLink to="/">Home</NavLink>
-              <NavLink to="/books">Books</NavLink>
-              <NavLink to="/search">Search</NavLink>
-              <NavLink to={`/profiles/${user.profile}`}>My Profile</NavLink>
-              <NavLink to="/auth/change-password">Change Password</NavLink>
-              <NavLink to="" onClick={handleLogout}>LOG OUT</NavLink>
+              <div className={styles.userDropDown}>
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/books">Books</NavLink>
+                <NavLink to="/search">Search</NavLink>
+                <NavLink to={`/profiles/${user.profile}`}>My Profile</NavLink>
+                <NavLink to="/auth/change-password">Change Password</NavLink>
+                <NavLink to="" onClick={handleLogout}>LOG OUT</NavLink>
+              </div>
             </div>
-          </div>
-        :
-          <div className={styles.userLinks}>
-            <i className="fa-solid fa-bars"></i>
-            <div className={styles.userDropDown}>
-              <NavLink to="/auth/login">Log In</NavLink>
-              <NavLink to="/auth/signup">Sign Up</NavLink>
+          :
+            <div className={styles.userLinks}>
+              <i className="fa-solid fa-bars"></i>
+              <div className={styles.userDropDown}>
+                <NavLink to="/auth/login">Log In</NavLink>
+                <NavLink to="/auth/signup">Sign Up</NavLink>
+              </div>
             </div>
-          </div>
-        }
+          }
+        </div>
       </div>
     </nav>
   )
