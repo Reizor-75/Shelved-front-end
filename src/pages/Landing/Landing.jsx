591b9f2254f695fc6d89fbc1da5804fa81154ff3
mlinares <m.linares93@gmail.com>
2024-04-23 21:51:26 -0400

    create state for recent releases

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,11 +1,33 @@
+//npm modules
+import { useEffect, useState } from 'react'
+
+//service
+import * as bookService from '../../services/bookService'
+
+//components
+import BookCard from '../../components/BookCard/BookCard'
+
 // css
 import styles from './Landing.module.css'
 
 const Landing = () => {
+  const [recent, setRecent] = useState()
+
+  useEffect(() =>{
+  }, [])
+
   return (
     <main className={styles.container}>
       <h1>Recent Releases</h1>
+      <div className={styles.slideContainer}>
+        {recent.map(book => (
+          <BookCard key={book.OLID} book={book}/>
+        ))}
+      </div>
       <h1>Fan Favorites</h1>
+      <div className={styles.slideContainer}>
+
+      </div>
     </main>
   )
 }
