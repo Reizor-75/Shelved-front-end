dbc7286c388743d75b52d18e069a0b0163d60fdd
mlinares <m.linares93@gmail.com>
2024-05-01 23:56:13 -0400

    style profile card component

--- src/components/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -1,13 +1,17 @@
-//npm module
+// npm module
 import { NavLink } from "react-router-dom";
+
+// css
+import styles from "./ProfileCard.module.css"
 
 const ProfileCard = ({profile}) => {
   return ( 
     <NavLink to={`/profiles/${profile._id}`}>
+      
       <img 
         src={profile.photo}
         alt={`${profile.name}'s avatar`} 
-        className={profile.profileAvatar}
+        className={styles.profileAvatar}
       />
     </NavLink>
     
